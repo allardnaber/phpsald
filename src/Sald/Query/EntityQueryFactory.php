9ec83233d81487b8653e9a0db722d2af5612782c
Allard Naber <a.l.naber@rug.nl>
2023-10-16 00:13:31 +0200

    termporary use whereLiteral before replacing with condition

--- src/Sald/Query/EntityQueryFactory.php
+++ src/Sald/Query/EntityQueryFactory.php
@@ -25,7 +25,7 @@ class EntityQueryFactory {
 	public static function update(Connection $connection, TableMetadata $metadata, Entity $entity): SimpleUpdateQuery {
 		$result = new SimpleUpdateQuery($connection, $metadata);
 		$idColumn = $metadata->getIdColumnName();
-		$result->where($idColumn . '=:' . $idColumn);
+		$result->whereLiteral($idColumn . '=:' . $idColumn);
 		$result->parameter($idColumn, $entity->$idColumn);
 
 		$updateFields = array_intersect($entity->getDirtyFields(), $metadata->getEditableFields());
@@ -40,7 +40,7 @@ class EntityQueryFactory {
 	public static function delete(Connection $connection, TableMetadata $metadata, Entity $entity): SimpleDeleteQuery {
 		$result = new SimpleDeleteQuery($connection, $metadata);
 		$idColumn = $metadata->getIdColumnName();
-		$result->where($idColumn . '=:' . $idColumn);
+		$result->whereLiteral($idColumn . '=:' . $idColumn);
 		$result->parameter($idColumn, $entity->$idColumn);
 		return $result;
 	}
