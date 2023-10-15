14d77c31957b3f2d1420ffef5b2c7ab46979fa54
Allard Naber <a.l.naber@rug.nl>
2023-10-15 15:45:59 +0200

    Add shortcut for select query

--- src/Sald/Sald.php
+++ src/Sald/Sald.php
@@ -5,11 +5,16 @@ namespace Sald;
 use Sald\Connection\Configuration;
 use Sald\Connection\Connection;
 use Sald\Connection\ConnectionManager;
+use Sald\Query\SimpleSelectQuery;
 
 class Sald {
 
-	public static function get(?Configuration $config): Connection {
+	public static function get(?Configuration $config = null): Connection {
 		return ConnectionManager::get($config);
+	}
+
+	public static function select(string $className, ?Configuration $config = null): SimpleSelectQuery {
+		return self::get($config)->select($className);
 	}
 
 }
