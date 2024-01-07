2cdd11acce2ef8553fb6e2dcc552909ba1bf86af
Allard Naber <allardnaber@gmail.com>
2024-01-07 23:20:18 +0100

    Use whereId to update/delete existing entities

--- src/Sald/Query/EntityQueryFactory.php
+++ src/Sald/Query/EntityQueryFactory.php
@@ -25,8 +25,7 @@ class EntityQueryFactory {
 	public static function update(Connection $connection, TableMetadata $metadata, Entity $entity): SimpleUpdateQuery {
 		$result = new SimpleUpdateQuery($connection, $metadata);
 		$idColumn = $metadata->getIdColumnName();
-		$result->whereLiteral($idColumn . '=:' . $idColumn);
-		$result->parameter($idColumn, $entity->$idColumn);
+		$result->whereId($entity->$idColumn);
 
 		$updateFields = array_intersect($entity->getDirtyFields(), $metadata->getEditableFields());
 
@@ -40,8 +39,7 @@ class EntityQueryFactory {
 	public static function delete(Connection $connection, TableMetadata $metadata, Entity $entity): SimpleDeleteQuery {
 		$result = new SimpleDeleteQuery($connection, $metadata);
 		$idColumn = $metadata->getIdColumnName();
-		$result->whereLiteral($idColumn . '=:' . $idColumn);
-		$result->parameter($idColumn, $entity->$idColumn);
+		$result->whereId($entity->$idColumn);
 		return $result;
 	}
 }
