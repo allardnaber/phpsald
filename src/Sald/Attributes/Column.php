3b0fb2962f6eabec9f4470b8a028e5889a8eed92
Allard Naber <allardnaber@gmail.com>
2025-09-30 21:53:08 +0200

    allow column types

--- src/Sald/Attributes/Column.php
+++ src/Sald/Attributes/Column.php
@@ -6,9 +6,18 @@ use Attribute;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class Column {
-	public function __construct(private readonly string $name) {}
+
+	const TYPE_UNDEFINED = 0;
+	const TYPE_JSON = 11;
+	const TYPE_JSONB = 12;
+
+	public function __construct(private readonly string $name, private readonly int $type = self::TYPE_UNDEFINED) {}
 
 	public function getColumnName(): string {
 		return $this->name;
+	}
+
+	public function getColumnType(): int {
+		return $this->type;
 	}
 }
