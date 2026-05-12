2b8d34fd450a367b68b98984eacba92d5f0d731d
Allard Naber <allardnaber@gmail.com>
2026-05-12 22:59:33 +0200

    support enum while inserting or updating

--- src/Sald/Entities/Mapper/GenericInputMapper.php
+++ src/Sald/Entities/Mapper/GenericInputMapper.php
@@ -8,13 +8,20 @@ use Sald\Query\QueryParameter;
 class GenericInputMapper extends InputMapper {
 
 	public function convertInput(QueryParameter $param): mixed {
-		$column = $this->getTableMetadata()->getColumn($param->getColumnName());
-		if ($column !== null && $column->getDbObjectType() === ColumnType::JSON) {
-			return json_encode($param->getValue());
+		$value = $param->getValue();
+
+		if ($value instanceof \UnitEnum) {
+			return $value->name;
 		}
 
+		$column = $this->getTableMetadata()->getColumn($param->getColumnName());
+		if ($column !== null && $column->getDbObjectType() === ColumnType::JSON) {
+			return json_encode($value);
+		}
+
+
 		// no translation needed
-		return $param->getValue();
+		return $value;
 	}
 
 }
