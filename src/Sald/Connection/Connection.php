414bf1512d5a0192d0b4f3a5a0a06cecf04144cd
Allard Naber <allardnaber@gmail.com>
2026-06-03 17:31:58 +0200

    remove argument

--- src/Sald/Connection/Connection.php
+++ src/Sald/Connection/Connection.php
@@ -37,7 +37,7 @@ class Connection extends PDO implements LoggerAwareInterface {
 	private function setSchema(?string $schema): void {
 		if ($schema !== null) {
 			$stmt = $this->prepare(sprintf('SET search_path to "%s"', addslashes($schema)));
-			$stmt->execute([$schema]);
+			$stmt->execute();
 		}
 	}
 
